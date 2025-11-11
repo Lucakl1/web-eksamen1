d38aa8838cab4400cd8a0af9f9dead85499611c9
Luca <141769892+Lucakl1@users.noreply.github.com>
2025-11-11 18:34:54 +0100

    test route

--- app.py
+++ app.py
@@ -27,6 +27,37 @@ def _____USER_____(): pass
 ##############################
 ##############################
 
+##############################
+##############################
+@app.route("/test", methods=["GET","POST"])
+@app.route("/test/<test_name>", methods=["GET","POST"])
+def view_tests(test_name = "all"):
+    try:
+        if test_name == "all":
+            all_the_html = ""
+            templates_path = os.listdir(os.path.join(app.root_path, 'templates/test'))
+            for file in templates_path:
+                all_the_html += render_template(f"test/{file}")
+            return all_the_html
+        return render_template(f"test/{test_name}.html")
+    except:
+        return "either no pages was found, or an error in the code"
+    
+@app.post('/upload')
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return 'No selected file'
+    
+    file.save(os.path.join("static/uploads", file.filename))
+    return redirect("/test")
+##############################
+##############################
+
 @app.get("/")
 def view_index():
     return render_template("index.html")
